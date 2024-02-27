a858f3a2a4e71ee6d9e2d08ec878e0e72915ccf2
Charles Carroll <ccarroll4@ycp.edu>
2024-02-26 22:14:44 -0500

    First commit

---
+++ FlappyBirdRemastered/FlappyBird.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import(
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+pygame.init()
+screen_width = 1000
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+pygame.quit()
